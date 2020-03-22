bcde1cefc03eee7ff98dfe14843e7bffae5e44de
Fedor <arbuzovfp@gmail.com>
2020-03-22 15:05:25 +0300

    fix

--- src/components/TextEditor.vue
+++ src/components/TextEditor.vue
@@ -4,9 +4,13 @@
 
 <script>
 import { codemirror } from 'vue-codemirror'
-
+  // language
+  import 'codemirror/mode/sql/sql.js'
   // base style
   import 'codemirror/lib/codemirror.css'
+
+    // theme css
+  import 'codemirror/theme/base16-light.css'
   
 export default {
     components: {
@@ -17,8 +21,8 @@ export default {
         code: 'const a = 10',
         cmOptions: {
             tabSize: 4,
-            mode: 'text/javascript',
-            theme: 'base16-dark',
+            mode: 'text/x-sql',
+            theme: 'base16-light',
             lineNumbers: true,
             line: true,
             // more CodeMirror options...
