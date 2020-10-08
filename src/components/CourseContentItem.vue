f8350d86edd94950915db37858a9abd3e79ecb69
Nikita <garret613@gmail.com>
2020-10-08 22:08:33 +0300

    cross added

--- src/components/CourseContentItem.vue
+++ src/components/CourseContentItem.vue
@@ -1,20 +1,19 @@
 <template>
   <div>
-    <li class="chapter-title" v-on:click="message">
+    <li class="chapter-title">
       {{ chapter.chapter_name }}
-    </li>
-    <div class="accordion-ul__container">
-    <svg class="accordion__icon" width="28" height="28">
-      <use xlink:href="#icon-plus"></use>
-    </svg>
-    <use xlink:href="#icon-plus">
-      <svg id="icon-plus" viewBox="0 0 16 16">
-        <path
-          d="M14.812 8a.62.62 0 0 1-.62.622h-5.57v5.57a.62.62 0 0 1-.622.62.62.62 0 0 1-.621-.62v-5.57h-5.57a.62.62 0 0 1-.62-.621.606.606 0 0 1 .618-.618h5.569v-5.57a.62.62 0 0 1 .18-.439.599.599 0 0 1 .438-.179.62.62 0 0 1 .622.621l.006 5.563h5.57c.344 0 .62.276.62.621z"
-          fill="currentColor"
-        ></path>
+      <svg class="accordion__icon" width="28" height="28" v-on:click="message">
+        <use xlink:href="#icon-plus"></use>
       </svg>
-    </use>
+      <use xlink:href="#icon-plus">
+        <svg id="icon-plus" viewBox="0 0 16 16">
+          <path
+            d="M14.812 8a.62.62 0 0 1-.62.622h-5.57v5.57a.62.62 0 0 1-.622.62.62.62 0 0 1-.621-.62v-5.57h-5.57a.62.62 0 0 1-.62-.621.606.606 0 0 1 .618-.618h5.569v-5.57a.62.62 0 0 1 .18-.439.599.599 0 0 1 .438-.179.62.62 0 0 1 .622.621l.006 5.563h5.57c.344 0 .62.276.62.621z"
+            fill="currentColor"
+          ></path>
+        </svg>
+      </use>
+    </li>
     <ul
       v-for="chapteritem in chapter.chapterItems"
       :key="chapteritem.title"
@@ -26,53 +25,42 @@
       >
         <a :href="generateUrl(course, chapteritem)">{{ chapteritem.title }}</a>
       </li>
-    </ul>
-    </div>
+    </ul>  
   </div>
 </template>
 
-<style>
-/* 1. найти и перенести стили из CourseContent. Перенести стили вдумчиво (exallenge.web.app)
-   2.  сделать крестик справа от списка и перенести функцию нажатия на него (командная строка)
-   3. можно убрать флекс из accordion-ul__container*/
+<style scoped>
+
 .hidden {
   display: none;
 }
 
 .accordion-ul__container {
   display: flex;
-  flex-direction: column;  
-  
+  flex-direction: column;
+  justify-self: center;
 }
 
 .accordion__icon {
-  border: 1px solid #000;
-  border-radius: 50%;
-  align-self: flex-end;
-  /* margin-left: 1000px; */
+ color: #a38b70;
+  
 }
 
 .chapter-title {
   font-size: 22px;
+  display: flex;
+  justify-content: space-between;
 }
 
 .chapter-item {
   font-size: 20px;
   line-height: 28px;
+  width: fit-content;
 }
 
 ul {
   list-style: none;
   display: flex;
-}
-
-ul li::before {
-  content: "\2022";
-  color: black;
-  font-weight: bold;
-  display: inline-block;
-  width: 1em;
-  margin-left: -1em;
 }
 
 ul li.chapter-item::before {
--- src/components/DescriptionCourse.vue
+++ src/components/DescriptionCourse.vue
@@ -2,21 +2,9 @@
   <div class="popular-and-price">
     <div class="container">
       <h2 class="section-title" v-html="courseInfo.name"></h2>
-      <a
-        class="btn to-all-reviews"
-        v-bind:href="courseInfo.lastTaskUrl"
-        title="Продолжить"
-        v-if="courseInfo.enrolled"
-        >Продолжить</a
-      >
-      <a
-        class="btn to-all-reviews"
-        :href="`/enroll/${this.$route.params.course}`"
-        title="Присоедениться"
-        v-else
-        >Присоедениться</a
-      >
-      <p>{{ courseInfo.description }}</p>
+      <a class="btn to-all-reviews" v-bind:href="courseInfo.lastTaskUrl" title="Продолжить" v-if="courseInfo.enrolled">Продолжить</a>
+      <a class="btn to-all-reviews" :href="`/enroll/${this.$route.params.course}`" title="Присоедениться" v-else>Присоедениться</a>
+      <p>{{ courseInfo.description}}</p>
       <CoursesContent :course="courseInfo" />
     </div>
   </div>
@@ -37,32 +25,26 @@
   padding-top: 190px;
   padding-bottom: 90px;
 }
-
 .container {
   width: 1000px;
   margin: 0 auto;
   position: relative;
 }
-
 .section-title {
   padding-left: 20px;
 }
-
 p {
   font-size: 22px;
   padding-left: 20px;
 }
-
 a {
   color: #a38b70;
   text-decoration: none;
 }
-
 .to-all-reviews {
   position: absolute;
   top: 0;
   right: 0;
-
   font-size: 18px;
   font-weight: 600;
   text-decoration: none;
@@ -71,22 +53,18 @@ a {
   border: 2px solid #a69f95;
   border-radius: 4px;
 }
-
 .to-all-reviews:hover {
   color: #7d6038;
   border: 2px solid #6c5330;
 }
-
 .to-all-reviews:active {
   color: #908b85;
   border: 2px solid #7d7973;
 }
-
 @media only screen and (max-width: 1050px) {
   .section-title {
     padding-left: 20px;
   }
-
   .popular-items {
     display: block;
   }
@@ -105,7 +83,6 @@ a {
     height: initial;
   }
 }
-
 @media screen and (max-width: 1020px) {
   .container {
     width: initial;
@@ -117,7 +94,6 @@ a {
     right: 20px;
   }
 }
-
 @media screen and (max-width: 750px) {
   .reviews-list {
     display: block;
@@ -131,7 +107,6 @@ a {
     border-left-color: transparent;
   }
 }
-
 @media screen and (max-width: 420px) {
   .to-all-reviews {
     display: none;
@@ -143,338 +118,52 @@ a {
 </style>
 
 <script>
-// import firebase from "firebase";
+import firebase from "firebase";
 import { mapGetters } from "vuex";
-
 import CoursesContent from "./CourseContent";
-
 import {
-  // getUserInfo,
-  // checkRouter,
-  convertCourseResponseStruncture,
+  getUserInfo,
+  checkRouter,
+  convertCourseResponseStruncture
 } from "../utils/getUserInfo";
-
 export default {
   components: {
-    CoursesContent,
+    CoursesContent
   },
   data() {
     return {
-      course: {
-      "1": {
-        "1": {
-          meta: {
-            task_type: "NT",
-            need_program_check: false,
-            need_teacher_check: false,
-            task_name: "Введение",
-          },
-        },
-        "2": {
-          meta: {
-            task_type: "NT",
-            need_program_check: false,
-            task_name: "Командная строка",
-            need_teacher_check: false,
-          },
-        },
-        "3": {
-          meta: {
-            need_teacher_check: false,
-            task_name: "Структура файловой системы",
-            task_type: "NT",
-            need_program_check: false,
-          },
-        },
-        "4": {
-          meta: {
-            need_teacher_check: false,
-            task_type: "NT",
-            task_name: "Вывод рабочего каталога (pwd)",
-            need_program_check: false,
-          },
-        },
-        "5": {
-          meta: {
-            need_teacher_check: false,
-            task_type: "NT",
-            task_name: "Список (ls)",
-            need_program_check: false,
-          },
-        },
-        "6": {
-          meta: {
-            need_program_check: false,
-            task_name: "Изменить каталог (cd)",
-            need_teacher_check: false,
-            task_type: "NT",
-          },
-        },
-        "7": {
-          meta: {
-            need_program_check: false,
-            need_teacher_check: false,
-            task_name: "Сделать директорию (mkdir)",
-            task_type: "NT",
-          },
-        },
-        "8": {
-          meta: {
-            task_name: "Создать новый файл (touch)",
-            task_type: "NT",
-            need_program_check: false,
-            need_teacher_check: false,
-          },
-        },
-        "9": {
-          meta: {
-            need_teacher_check: false,
-            task_name: "Итоги",
-            task_type: "NT",
-            need_program_check: false,
-          },
-        },
-        meta: {
-          description: "",
-          chapter_name: "Навигация по файловой системе",
-        },
-      },
-      "2": {
-        "1": {
-          meta: {
-            task_type: "NT",
-            need_teacher_check: false,
-            need_program_check: false,
-            task_name: "Параметры команд",
-          },
-        },
-        "2": {
-          meta: {
-            task_type: "NT",
-            task_name: "Параметры команды ls",
-            need_teacher_check: false,
-            need_program_check: false,
-          },
-        },
-        "3": {
-          meta: {
-            need_teacher_check: false,
-            task_name: "Копировать (cd)",
-            need_program_check: false,
-            task_type: "NT",
-          },
-        },
-        "4": {
-          meta: {
-            need_teacher_check: false,
-            task_type: "NT",
-            task_name: "Копировать (cd)",
-            need_program_check: false,
-          },
-        },
-        "5": {
-          meta: {
-            task_type: "NT",
-            task_name: "Удалить (rm)",
-            need_teacher_check: false,
-            need_program_check: false,
-          },
-        },
-        "6": {
-          meta: {
-            need_teacher_check: false,
-            task_name: "Итоги",
-            need_program_check: false,
-            task_type: "NT",
-          },
-        },
-        meta: {
-          description: "",
-          chapter_name: "Просмотр и изменение элементов файловой системы",
-        },
-      },
-      meta: {
-        promo_code: "HSE",
-        price: "0",
-        description:
-          "Командная строка - это быстрый, мощный текстовый интерфейс, который разработчики используют для более эффективной и действенной связи с компьютерами для выполнения более широкого набора задач.",
-        course_name: "Командная строка.<br />Базовый курс",
-      },
-    },
+      course: {}
     };
   },
   computed: {
     ...mapGetters({
-      user: "user",
+      user: "user"
     }),
-
     courseInfo() {
       return convertCourseResponseStruncture(
         this.course,
         this.user,
         this.$route.params.course
       );
-    },
+    }
   },
   beforeCreate: function() {
-    this.course = {
-      "1": {
-        "1": {
-          meta: {
-            task_type: "NT",
-            need_program_check: false,
-            need_teacher_check: false,
-            task_name: "Введение",
-          },
-        },
-        "2": {
-          meta: {
-            task_type: "NT",
-            need_program_check: false,
-            task_name: "Командная строка",
-            need_teacher_check: false,
-          },
-        },
-        "3": {
-          meta: {
-            need_teacher_check: false,
-            task_name: "Структура файловой системы",
-            task_type: "NT",
-            need_program_check: false,
-          },
-        },
-        "4": {
-          meta: {
-            need_teacher_check: false,
-            task_type: "NT",
-            task_name: "Вывод рабочего каталога (pwd)",
-            need_program_check: false,
-          },
-        },
-        "5": {
-          meta: {
-            need_teacher_check: false,
-            task_type: "NT",
-            task_name: "Список (ls)",
-            need_program_check: false,
-          },
-        },
-        "6": {
-          meta: {
-            need_program_check: false,
-            task_name: "Изменить каталог (cd)",
-            need_teacher_check: false,
-            task_type: "NT",
-          },
-        },
-        "7": {
-          meta: {
-            need_program_check: false,
-            need_teacher_check: false,
-            task_name: "Сделать директорию (mkdir)",
-            task_type: "NT",
-          },
-        },
-        "8": {
-          meta: {
-            task_name: "Создать новый файл (touch)",
-            task_type: "NT",
-            need_program_check: false,
-            need_teacher_check: false,
-          },
-        },
-        "9": {
-          meta: {
-            need_teacher_check: false,
-            task_name: "Итоги",
-            task_type: "NT",
-            need_program_check: false,
-          },
-        },
-        meta: {
-          description: "",
-          chapter_name: "Навигация по файловой системе",
-        },
-      },
-      "2": {
-        "1": {
-          meta: {
-            task_type: "NT",
-            need_teacher_check: false,
-            need_program_check: false,
-            task_name: "Параметры команд",
-          },
-        },
-        "2": {
-          meta: {
-            task_type: "NT",
-            task_name: "Параметры команды ls",
-            need_teacher_check: false,
-            need_program_check: false,
-          },
-        },
-        "3": {
-          meta: {
-            need_teacher_check: false,
-            task_name: "Копировать (cd)",
-            need_program_check: false,
-            task_type: "NT",
-          },
-        },
-        "4": {
-          meta: {
-            need_teacher_check: false,
-            task_type: "NT",
-            task_name: "Копировать (cd)",
-            need_program_check: false,
-          },
-        },
-        "5": {
-          meta: {
-            task_type: "NT",
-            task_name: "Удалить (rm)",
-            need_teacher_check: false,
-            need_program_check: false,
-          },
-        },
-        "6": {
-          meta: {
-            need_teacher_check: false,
-            task_name: "Итоги",
-            need_program_check: false,
-            task_type: "NT",
-          },
-        },
-        meta: {
-          description: "",
-          chapter_name: "Просмотр и изменение элементов файловой системы",
-        },
-      },
-      meta: {
-        promo_code: "HSE",
-        price: "0",
-        description:
-          "Командная строка - это быстрый, мощный текстовый интерфейс, который разработчики используют для более эффективной и действенной связи с компьютерами для выполнения более широкого набора задач.",
-        course_name: "Командная строка.<br />Базовый курс",
-      },
-    };
-
-    // getUserInfo();
-    // firebase.auth().onAuthStateChanged(user => {
-    //   this.$store.dispatch("fetchUser", user);
-    //   checkRouter();
-    // });
-    // firebase
-    //   .firestore()
-    //   .collection("coursesContent")
-    //   .doc(this.$route.params.course)
-    //   .get()
-    //   .then(doc => {
-    //     this.course = doc.data();
-    //   })
-    //   .catch(err => {
-    //     alert("Error getting documents", err);
-    //   });
-  },
+    getUserInfo();
+    firebase.auth().onAuthStateChanged(user => {
+      this.$store.dispatch("fetchUser", user);
+      checkRouter();
+    });
+    firebase
+      .firestore()
+      .collection("coursesContent")
+      .doc(this.$route.params.course)
+      .get()
+      .then(doc => {
+        this.course = doc.data();
+      })
+      .catch(err => {
+        alert("Error getting documents", err);
+      });
+  }
 };
 </script>
